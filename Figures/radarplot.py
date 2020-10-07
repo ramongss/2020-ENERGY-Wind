ec469fb8bb59f7c2b2e16d2411a1dc2127d2ca7f
Ramon G. da Silva <ramongs1406@gmail.com>
2020-10-06 23:13:24 -0300

    Update codes, plot new figures, and update README.md

--- Figures/radarplot.py
+++ Figures/radarplot.py
@@ -1,21 +1,26 @@
 # Libraries
 import matplotlib.pyplot as plt
+#from matplotlib import PolarAxesSubplot
 import pandas as pd
 from math import pi
 
 months = ['August', 'September', 'October']
 
-for ii in range(1, 4):
+fig = plt.figure(figsize=(20.25,6.75))
+fig, axs = plt.subplots(nrows=1, ncols=3, subplot_kw=dict(projection='polar'))
+plt.subplots_adjust(wspace=0.2)
+
+for ii in range(1,4):
     # Set data
     df = pd.read_csv('dataset{}_std.csv'.format(ii))
-
+    
     # create a color palette
     palette = plt.get_cmap('Set1')
 
     # ------- PART 1: Create background
 
     # number of variable
-    categories = list(df)
+    categories=list(df)
     N = len(categories)
 
     # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
@@ -23,20 +28,25 @@ for ii in range(1, 4):
     angles += angles[:1]
 
     # Initialise the spider plot
-    fig = plt.figure(figsize=(6.75, 6.75))
-    ax = plt.subplot(111, polar=True)
+    #fig = plt.figure(figsize=(6.75,6.75))
+    #ax = plt.subplot(111, polar=True)
+    ax = axs[ii - 1]
 
     # If you want the first axis to be on top:
     ax.set_theta_offset(pi / 2)
     ax.set_theta_direction(-1)
 
     # Draw one axe per variable + add labels labels yet
-    plt.xticks(angles[:-1], categories)
+    ax.set_xticks(ticks=angles[:-1])
+    ax.set_xticklabels(categories, fontdict={'fontsize':12})
 
-    # Draw ylabels
     ax.set_rlabel_position(0)
-    plt.yticks(color="grey", size=8)
-    plt.ylim(df.values.min()-10, df.values.max()+10)
+    ticks=ax.yaxis.get_major_ticks()
+    for tick in ticks:
+        tick.label1.set_color('grey')
+        tick.label1.set_size(10)
+    ax.set_ylim(df.values.min()-10,df.values.max()+10)
+
 
     # ------- PART 2: Add plots
 
@@ -44,33 +54,33 @@ for ii in range(1, 4):
     # I don't do a loop, because plotting more than 3 groups makes the chart unreadable
 
     # Ind1
-    values = df.loc[0].values.flatten().tolist()
+    values=df.loc[0].values.flatten().tolist()
     values += values[:1]
-    ax.plot(angles, values, color=palette(1), linewidth=1,
-            linestyle='solid', label="10 minutes")
+    ax.plot(angles, values, color=palette(1), linewidth=2, linestyle='solid', label="10 minutes")
     ax.fill(angles, values, color=palette(1), alpha=0.1)
 
     # Ind2
-    values = df.loc[1].values.flatten().tolist()
+    values=df.loc[1].values.flatten().tolist()
     values += values[:1]
-    ax.plot(angles, values, color=palette(0), linewidth=1,
-            linestyle='solid', label="20 minutes")
+    ax.plot(angles, values, color=palette(0), linewidth=2, linestyle='solid', label="20 minutes")
     ax.fill(angles, values, color=palette(0), alpha=0.1)
-
+    
     # Ind3
-    values = df.loc[2].values.flatten().tolist()
+    values=df.loc[2].values.flatten().tolist()
     values += values[:1]
-    ax.plot(angles, values, color=palette(4), linewidth=1,
-            linestyle='solid', label="30 minutes")
+    ax.plot(angles, values, color=palette(4), linewidth=2, linestyle='solid', label="30 minutes")
     ax.fill(angles, values, color=palette(4), alpha=0.1)
 
-    # Add legend
-    # plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1), fontsize=12) # legend at bottom left
-    plt.legend(loc='lower left', bbox_to_anchor=(0.8, 0.9),
-               fancybox=True, shadow=True, fontsize=12)  # legend at up left
-    plt.title(months[ii-1], fontsize=20)
+    # Set title
+    ax.set_title(months[ii-1], fontsize=20, pad=20)
 
-    # plt.savefig('radaplot_dataset{}.png'.format(ii), dpi=300, bbox_inches='tight', pad_inches=0)
-    plt.savefig('radaplot_dataset{}.pdf'.format(ii),
-                dpi=300, bbox_inches='tight', pad_inches=0)
-    # plt.show()
+# Add legend
+# plt.legend(loc='lower left', bbox_to_anchor=(-1.5, -0.2), fontsize=16, ncol=3) # legend at up left
+handles, labels = ax.get_legend_handles_labels()
+# #fig.legend(handles, labels, bbox_to_anchor=(0.8,0.9), loc='lower left', fontsize=6)
+fig.legend(handles, labels, ncol = 3, loc='center', bbox_to_anchor=(0.15, -0.2, 0.5, 0.5), fontsize=16)
+
+fig.set_size_inches(20.25, 6.75)
+# fig.savefig('radaplot.png',dpi=1200, bbox_inches='tight', pad_inches=0)
+fig.savefig('radaplot.pdf',dpi=1200, bbox_inches='tight', pad_inches=0)
+# plt.show()
--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 ## :zap: A novel decomposition-ensemble learning framework for multi-step ahead wind energy forecasting
 [<img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon"></img> Ramon Gomes da Silva][RamonID], [<img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon"></img> Matheus H. D. M. Ribeiro][MatheusID], [<img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon"></img> Sinvaldo Rodrigues Moreno][SinvaldoID], [<img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon"></img> Viviana Cocco Mariani][VivianaID], [<img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon"></img> Leandro dos Santos Coelho][LeandroID]
 
-Dataset 1 | Dataset 2 | Dataset 3
-:---:|:---:|:---:
-![Predictions1](https://user-images.githubusercontent.com/47225177/91241215-a3b5fb80-e71a-11ea-8d90-82b918285710.png) | ![Predictions2](https://user-images.githubusercontent.com/47225177/91241250-bcbeac80-e71a-11ea-8cb2-985d6b6d1199.png) | ![Predictions3](https://user-images.githubusercontent.com/47225177/91241271-d06a1300-e71a-11ea-8273-4cfbe1941ced.png) |
-![Radar plot1](https://user-images.githubusercontent.com/47225177/91241412-3191e680-e71b-11ea-8bd8-ff75707d1837.png)  | ![Radar plot2](https://user-images.githubusercontent.com/47225177/91241435-3d7da880-e71b-11ea-9a80-93e139564350.png)  | ![Radar plot3](https://user-images.githubusercontent.com/47225177/91241443-45d5e380-e71b-11ea-8f53-9c8f58b47724.png)  |
+![Proposed Forecasting Framework](Figures/framework.png)|
+:---|
+**_Proposed forecasting framework_**|
 
 ### Publication
 - [Preprint (ResearchGate)]()
